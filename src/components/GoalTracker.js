5e8efe41d536161eee205160a1c11106683f8326
Ossiggy <mrossig201@gmail.com>
2018-08-01 14:48:57 -0400

    add-a-goal button is not a dropdown with options that will further define the goal that is being added

--- src/components/GoalTracker.js
+++ src/components/GoalTracker.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import {ProgressBar, Grid, Row, Col} from 'react-bootstrap';
+import {ProgressBar, DropdownButton, MenuItem, Grid, Row, Col} from 'react-bootstrap';
 import GoalContainer from './GoalContainer';
 
 export default class GoalTracker extends Component{
@@ -16,11 +16,27 @@ export default class GoalTracker extends Component{
     return(
       <Grid>
         <Row className="total-progress">
-          <Col md={2}>
-            Total Progress
+          <Col md={4}>
+          <h1 style={{margin: 0, fontSize: "28px"}}>Total Progress</h1>
           </Col>
           <Col md={6}>
-            <ProgressBar active bsStyle="success" now={this.state.now} label={`${this.state.now}%`} />
+            <ProgressBar 
+              active 
+              bsStyle="success" 
+              now={this.state.now} 
+              style={{height: "30px", fontSize: "24px"}}
+              />
+          </Col>
+          <Col md={2} className="pull-right">
+            <DropdownButton 
+            bsStyle="success"
+            title="Add a Goal"
+            id="add-a-goal-button"
+            >
+            <MenuItem eventKey="1">Incremental</MenuItem>
+            <MenuItem eventKey="2">Timed</MenuItem>
+            <MenuItem eventKey="3">One-and-Done</MenuItem>
+            </DropdownButton>
           </Col>
         </Row>
         <Row className="active-goals">
