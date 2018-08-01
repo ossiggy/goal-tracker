5bcd3e7d1ffc33a1d74db9f273a01d14663ed9dd
Ossiggy <mrossig201@gmail.com>
2018-08-01 14:46:31 -0400

    changed styling of buttons

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -6,12 +6,16 @@ export default function Goal(props){
   return(
     <Grid fluid={true}>
       <Row className="goal-container">
-        <Col className="goal-container-name" md={12}>{props.name}</Col>
-        <Col className="goal-container-goal" md={12}>{props.goal}</Col>
+        <Col className="goal-container-name" md={12}>
+          <h3>{props.name}</h3>
+        </Col>
+        <Col className="goal-container-goal" md={12}>
+          <h4>{props.goal}</h4>
+        </Col>
       </Row>
       <Row className="goal-progress">
         <Col md={6}>
-          <ProgressBar active bsStyle="success" now={props.now} label={`${props.now}%`} />
+          <ProgressBar bsStyle="success" now={props.now} />
         </Col>
       </Row>
       <Row className="button-container">
