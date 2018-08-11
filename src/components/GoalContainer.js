a6c89ade777be073353304eba8d1e7e95752bfd4
Ossiggy <mrossig201@gmail.com>
2018-08-11 19:02:12 -0400

    changed to function rather than class since this will no longer have its own state

--- src/components/GoalContainer.js
+++ src/components/GoalContainer.js
@@ -1,33 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import Goal from './Goal';
 
-export default class GoalContainer extends Component{
-
-  constructor(props){
-    super(props);
-    this.state={
-      goals: [
-        {name: "Car Sales", goal: "Sell 204 Cars", now: 35}, 
-        {name: "Go to Gym", goal: "Go to the gym 3 times this week", now: 66}
-      ]
-    }
-  }
-
-  render(){
-
-    let goals;
-
-    if(Object.keys(this.state.goals)){
-      goals = this.state.goals.map((goal, i) => {
-        return <Goal key= {i} name={goal.name} goal={goal.goal} now={goal.now} />
+export default function GoalContainer(props){
+  
+  let goals;
+  props.goals.forEach(goal => {
+    if(Object.keys(goal)){
+      goals = props.goals.map((goal, i) => {
+        return <Goal key= {i} name={goal.name} type={goal.type} goal={goal.goal} now={goal.now} />
       })
     }
+})
     
     return(
+
       <div className="goal-container container">
         {goals}
       </div>
     );
-  };
 };
