70c51ff433c1d3697019f3d12a725034c9ecdb96
Ossiggy <mrossig201@gmail.com>
2018-08-15 20:46:03 -0400

    updated state so that daily would work, changed dividedBy values to ints rather than strings, started thinking of a function that will time the goals based off of user inputs

--- src/components/modals/Timed.js
+++ src/components/modals/Timed.js
@@ -17,7 +17,7 @@ constructor(props){
     timeFrame: '',
     quantity:"1",
     additionalInput:'',
-    dividedBy:'',
+    dividedBy:"1",
     startDate: moment(),
     touched: {
       name: false,
@@ -33,66 +33,95 @@ constructor(props){
   this.changeStartDate = this.changeStartDate.bind(this);
 }
 
-    handleBlur = (field) => (evt) => {
-      this.setState({
-        touched: {...this.state.touched, [field]:true}
-      })
-    }
+  handleBlur = (field) => (evt) => {
+    this.setState({
+      touched: {...this.state.touched, [field]:true}
+    })
+  }
 
-    handleInputChange(event) {
-      const target = event.target;
-      const value = target.value;
-      const name = target.name;
-      if(name==='quantity'){
-        console.log(value)
-      }
-      this.setState({[name]: value});
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    if(name==='dividedBy'){
+      console.log(value)
     }
+    this.setState({[name]: value});
+  }
 
-    changeStartDate(date){
-      this.setState({
-        startDate: date
-      });
-    }
+  changeStartDate(date){
+    this.setState({
+      startDate: date
+    });
+  }
 
-    appendInput(type){
-      if(type==="year"){
-        this.setState({additionalInput: "yearly"})
-      };
-      if(type==="month"){
-        this.setState({additionalInput: "monthly"})
-      };
-      if(type==="week"){
-        this.setState({additionalInput: "weekly"})
-      };
-      if(type==="day"){
-        this.setState({additionalInput: "daily"})
-      };
-    }
+  appendInput(type){
+    if(type==="year"){
+      this.setState({additionalInput: "yearly"})
+    };
+    if(type==="month"){
+      this.setState({additionalInput: "monthly"})
+    };
+    if(type==="week"){
+      this.setState({additionalInput: "weekly"})
+    };
+    if(type==="day"){
+      this.setState({additionalInput: "daily"})
+    };
+  }
 
-    addGoal(event) {
-      const state = this.state
-      const keys = Object.keys(state)
-      for(let i=0; i<keys.length; i++){
-        let key=keys[i];
-        for(key in state){
-          if(state[key]===null||state[key]===undefined){
-            console.log('undefined')
-          }
-        }
-      }
-      if(this.props.addGoal){
-        console.log(state.startDate._d)
-      }
-    }
+  // if monthly, use date array to check if it has been a month for goal to complete
+  // if month judged weekly, I have to find a way to see how many weeks are left in the month
+  // maybe set beginning and end range of how long the month is
+  // the date array gives me information on the month, days of the week, and the date
 
-    canBeSubmitted() {
-      const errors = validateTimed(this.state);
-      const isDisabled = Object.keys(errors).some(x => errors[x]);
-      return !isDisabled;
+  checkDateInfo(date){
+
+  }
+
+  addGoal(event) {
+
+    // function leapYear(year)
+    // {
+    //   return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+    // }
+
+    const state = this.state;
+    // const month = state.startDate._d.toString().split(" ")[1];
+    // const year = state.startDate._d.toString().split(" ")[3];
+    // const isLeapYear = leapYear(year);
+
+    // const thirtyDays = ["Sep", "Apr", "Jun", "Nov"];
+    // const feb = "Feb";
+
+    // if(month===feb&&state.additionalInput==="monthly"&&state.dividedBy==="31"&&isLeapYear){
+    //   this.setState({dividedBy: "29"})
+    // }
+
+    // if(month===feb&&state.additionalInput==="monthly"&&state.dividedBy==="31"&&!isLeapYear){
+    //   this.setState({dividedBy: "28"})
+    // }
+    // console.log(year, isLeapYear, state.dividedBy)
+    // type: "Timed",
+    // name: "Code Everyday",
+    // goal: "1 week",
+    // target: 7,
+    // actual: 7,
+
+   
+    if(this.props.addGoal){
+      console.log(state.startDate._d.toString().split(" "))
     }
+  }
+
+  canBeSubmitted() {
+    const errors = validateTimed(this.state);
+    const isDisabled = Object.keys(errors).some(x => errors[x]);
+    return !isDisabled;
+  }
 
   render(){
+
     const shouldMarkError = (field) => {
       const hasError = errors[field];
       const shouldShow = this.state.touched[field];
@@ -116,10 +145,10 @@ constructor(props){
           onBlur={this.handleBlur('dividedBy')}
           >
             <option value="undefined">Judge Progress</option>
-            <option value="year">Yearly</option>
-            <option value="month">Monthly</option>
-            <option value="week">Weekly</option>
-            <option value="day">Daily</option>
+            <option value="1">Yearly</option>
+            <option value="12">Monthly</option>
+            <option value="52">Weekly</option>
+            <option value="365">Daily</option>
           </select>
         );
       }
@@ -133,9 +162,9 @@ constructor(props){
           onBlur={this.handleBlur('dividedBy')}
           >
             <option value="undefined">Judge Progress</option>
-            <option value="month">Monthly</option>
-            <option value="week">Weekly</option>
-            <option value="day">Daily</option>
+            <option value="1">Monthly</option>
+            <option value="4">Weekly</option>
+            <option value="31">Daily</option>
           </select>
         );        
       }
@@ -149,8 +178,8 @@ constructor(props){
           onBlur={this.handleBlur('dividedBy')}
           >
             <option value="undefined">Judge Progress</option>
-            <option value="week">Weekly</option>
-            <option value="day">Daily</option>
+            <option value="1">Weekly</option>
+            <option value="7">Daily</option>
           </select>
         );        
       }
@@ -161,6 +190,7 @@ constructor(props){
     }
 
     return(
+
       <Modal show={this.props.show} onHide={this.props.onHide}>
         <Modal.Header closeButton style={{backgroundColor:"lightblue", color:"white"}}>
           <Modal.Title id="timed-modal-title" elementtype='h1'>Timed</Modal.Title>
